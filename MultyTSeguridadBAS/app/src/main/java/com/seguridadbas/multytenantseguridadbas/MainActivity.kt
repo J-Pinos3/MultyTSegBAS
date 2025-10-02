69451e661f44c17a82da53aa4d1b6062486fbafb
J-Pinos3 <josloco665@gmail.com>
2025-10-02 15:01:58 -0700

    Initial commit

---
+++ MultyTSeguridadBAS/app/src/main/java/com/seguridadbas/multytenantseguridadbas/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.seguridadbas.multytenantseguridadbas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.seguridadbas.multytenantseguridadbas.ui.theme.MultyTenantSeguridadBASTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MultyTenantSeguridadBASTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MultyTenantSeguridadBASTheme {
+        Greeting("Android")
+    }
+}
